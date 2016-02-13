3a1ebfeef8997e3bdab98ef0134b92341b753248
Sixte de Maupeou D'Ableiges <sd4215@doc.ic.ac.uk>
2016-02-13 13:57:06 +0000

    Added ids

--- app/assets/js/angularApp.js
+++ app/assets/js/angularApp.js
@@ -108,14 +108,17 @@ game.controller('gameController', function($scope) {
       src: "img/buttons/healerIcon.png",
       glyphicons: [
         {
+          id: "healer_plus",
           labelCount: 0,
           glyph: "plus"
         },
         {
+          id: "healer_forward",
           labelCount: 0,
           glyph: "forward"
         },
         {
+          id: "healer_dashboard",
           labelCount: 0,
           glyph: "dashboard"
         }
@@ -126,14 +129,17 @@ game.controller('gameController', function($scope) {
       src: "img/buttons/wallIcon.png",
       glyphicons: [
         {
+          id: "wall_plus",
           labelCount: 0,
           glyph: "plus"
         },
         {
+          id: "wall_forward",
           labelCount: 0,
           glyph: "forward"
         },
         {
+          id: "wall_dashboard",
           labelCount: 0,
           glyph: "dashboard"
         }
@@ -144,18 +150,22 @@ game.controller('gameController', function($scope) {
       src: "img/buttons/towerIcon.png",
       glyphicons: [
         {
+          id: "tower_plus",
           labelCount: 0,
           glyph: "plus"
         },
         {
+          id: "tower_forward",
           labelCount: 0,
           glyph: "forward"
         },
         {
+          id: "tower_dashboard",
           labelCount: 0,
           glyph: "dashboard"
         },
         {
+          id: "tower_flash",
           labelCount: 0,
           glyph: "flash"
         }
