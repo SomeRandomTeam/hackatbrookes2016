46e64848962089e183af8b3395541aef3cfacd86
Abhinav Mishra <abhinav.mishra15@imperial.ac.uk>
2016-02-13 11:58:46 +0000

    Angular Integrated.

--- app/assets/js/angularApp.js
+++ app/assets/js/angularApp.js
@@ -1,3 +1,165 @@
 var game = angular.module('game', []);
 
-game.controller()
+game.controller('gameController', function($scope) {
+  $scope.panel = [
+    {
+      src: "img/buttons/tankIcon.png",
+      glyphicons: [
+        {
+          labelCount: 0,
+          glyph: "plus"
+        },
+        {
+          labelCount: 0,
+          glyph: "forward"
+        },
+        {
+          labelCount: 0,
+          glyph: "dashboard"
+        }
+      ]
+
+    },
+    {
+      src: "img/buttons/chopperIcon.png",
+      glyphicons: [
+        {
+          labelCount: 0,
+          glyph: "plus"
+        },
+        {
+          labelCount: 0,
+          glyph: "forward"
+        },
+        {
+          labelCount: 0,
+          glyph: "dashboard"
+        },
+        {
+          labelCount: 0,
+          glyph: "flash"
+        }
+      ]
+
+    },
+    {
+      src: "img/buttons/rocketIcon.png",
+      glyphicons: [
+        {
+          labelCount: 0,
+          glyph: "plus"
+        },
+        {
+          labelCount: 0,
+          glyph: "forward"
+        },
+        {
+          labelCount: 0,
+          glyph: "dashboard"
+        },
+        {
+          labelCount: 0,
+          glyph: "flash"
+        }
+      ]
+
+    },
+    {
+      src: "img/buttons/bombIcon.png",
+      glyphicons: [
+        {
+          labelCount: 0,
+          glyph: "plus"
+        },
+        {
+          labelCount: 0,
+          glyph: "forward"
+        },
+        {
+          labelCount: 0,
+          glyph: "dashboard"
+        },
+        {
+          labelCount: 0,
+          glyph: "flash"
+        }
+      ]
+
+    },
+    {
+      src: "img/buttons/builderIcon.png",
+      glyphicons: [
+        {
+          labelCount: 0,
+          glyph: "plus"
+        },
+        {
+          labelCount: 0,
+          glyph: "forward"
+        },
+        {
+          labelCount: 0,
+          glyph: "dashboard"
+        }
+      ]
+
+    },
+    {
+      src: "img/buttons/healerIcon.png",
+      glyphicons: [
+        {
+          labelCount: 0,
+          glyph: "plus"
+        },
+        {
+          labelCount: 0,
+          glyph: "forward"
+        },
+        {
+          labelCount: 0,
+          glyph: "dashboard"
+        }
+      ]
+
+    },
+    {
+      src: "img/buttons/wallIcon.png",
+      glyphicons: [
+        {
+          labelCount: 0,
+          glyph: "plus"
+        },
+        {
+          labelCount: 0,
+          glyph: "forward"
+        },
+        {
+          labelCount: 0,
+          glyph: "dashboard"
+        }
+      ]
+
+    },
+    {
+      src: "img/buttons/towerIcon.png",
+      glyphicons: [
+        {
+          labelCount: 0,
+          glyph: "plus"
+        },
+        {
+          labelCount: 0,
+          glyph: "forward"
+        },
+        {
+          labelCount: 0,
+          glyph: "dashboard"
+        },
+        {
+          labelCount: 0,
+          glyph: "flash"
+        }
+      ]
+
+    }];
+});
