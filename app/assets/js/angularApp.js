f85350add6245bb755a187437b5dc6237a2fbe25
Abhinav Mishra <abhinav.mishra15@imperial.ac.uk>
2016-02-13 13:54:36 +0000

    Add Alerts and Inc. IDS for Buttons.

--- app/assets/js/angularApp.js
+++ app/assets/js/angularApp.js
@@ -4,16 +4,20 @@ game.controller('gameController', function($scope) {
   $scope.panel = [
     {
       src: "img/buttons/tankIcon.png",
+      blocked: "alert alert-success",
       glyphicons: [
         {
+          id: "tank_plus",
           labelCount: 0,
           glyph: "plus"
         },
         {
+          id: "tank_forward",
           labelCount: 0,
           glyph: "forward"
         },
         {
+          id: "tank_dashboard",
           labelCount: 0,
           glyph: "dashboard"
         }
@@ -22,20 +26,25 @@ game.controller('gameController', function($scope) {
     },
     {
       src: "img/buttons/chopperIcon.png",
+      blocked: "alert alert-danger",
       glyphicons: [
         {
+          id: "chopper_plus",
           labelCount: 0,
           glyph: "plus"
         },
         {
+          id: "chopper_forward",
           labelCount: 0,
           glyph: "forward"
         },
         {
+          id: "chopper_dashboard",
           labelCount: 0,
           glyph: "dashboard"
         },
         {
+          id: "chopper_flash",
           labelCount: 0,
           glyph: "flash"
         }
@@ -44,20 +53,25 @@ game.controller('gameController', function($scope) {
     },
     {
       src: "img/buttons/rocketIcon.png",
+      blocked: "alert alert-success",
       glyphicons: [
         {
+          id: "rocket_plus",
           labelCount: 0,
           glyph: "plus"
         },
         {
+          id: "rocket_forward",
           labelCount: 0,
           glyph: "forward"
         },
         {
+          id: "rocket_dashboard",
           labelCount: 0,
           glyph: "dashboard"
         },
         {
+          id: "rocket_flash",
           labelCount: 0,
           glyph: "flash"
         }
@@ -66,20 +80,25 @@ game.controller('gameController', function($scope) {
     },
     {
       src: "img/buttons/bombIcon.png",
+      blocked: "alert alert-danger",
       glyphicons: [
         {
+          id: "bomb_plus",
           labelCount: 0,
           glyph: "plus"
         },
         {
+          id: "bomb_forward",
           labelCount: 0,
           glyph: "forward"
         },
         {
+          id: "bomb_dashboard",
           labelCount: 0,
           glyph: "dashboard"
         },
         {
+          id: "bomb_flash",
           labelCount: 0,
           glyph: "flash"
         }
@@ -88,16 +107,20 @@ game.controller('gameController', function($scope) {
     },
     {
       src: "img/buttons/builderIcon.png",
+      blocked: "alert alert-success",
       glyphicons: [
         {
+          id: "builder_plus",
           labelCount: 0,
           glyph: "plus"
         },
         {
+          id: "builder_forward",
           labelCount: 0,
           glyph: "forward"
         },
         {
+          id: "builder_dashboard",
           labelCount: 0,
           glyph: "dashboard"
         }
@@ -106,6 +129,7 @@ game.controller('gameController', function($scope) {
     },
     {
       src: "img/buttons/healerIcon.png",
+      blocked: "alert alert-danger",
       glyphicons: [
         {
           labelCount: 0,
@@ -124,6 +148,7 @@ game.controller('gameController', function($scope) {
     },
     {
       src: "img/buttons/wallIcon.png",
+      blocked: "alert alert-success",
       glyphicons: [
         {
           labelCount: 0,
@@ -142,6 +167,7 @@ game.controller('gameController', function($scope) {
     },
     {
       src: "img/buttons/towerIcon.png",
+      blocked: "alert alert-danger",
       glyphicons: [
         {
           labelCount: 0,
