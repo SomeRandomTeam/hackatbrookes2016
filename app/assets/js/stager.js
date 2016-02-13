c67b67e1b9836d5a8531a4e28260e0a4044e33b3
Abhinav Mishra <abhinav.mishra15@imperial.ac.uk>
2016-02-13 13:20:42 +0000

    Proper factors.

--- app/assets/js/stager.js
+++ app/assets/js/stager.js
@@ -247,8 +247,8 @@ function createSprite(type, c_x, c_y, dir_x, dir_y) {
   newSprite.position.x = c_x;
   newSprite.position.y = c_y;
 
-  newSprite.height = window.innerHeight / 40;
-  newSprite.width = window.innerHeight / 40;
+  newSprite.height = window.innerHeight / 30;
+  newSprite.width = window.innerHeight / 30;
 
   newSprite.FRAMES = FRAMES;
   return  newSprite;
