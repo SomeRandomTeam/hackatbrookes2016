540c298ccc54080200a924121e0b9d830e65406f
Sixte de Maupeou D'Ableiges <sd4215@doc.ic.ac.uk>
2016-02-13 12:27:09 +0000

    Reduced size of sprites

--- app/assets/js/stager.js
+++ app/assets/js/stager.js
@@ -233,8 +233,8 @@ function createSprite(type, c_x, c_y, dir_x, dir_y) {
   newSprite.position.x = c_x;
   newSprite.position.y = c_y;
 
-  newSprite.height = window.innerHeight / 15;
-  newSprite.width = window.innerHeight / 15;
+  newSprite.height = window.innerHeight / 25;
+  newSprite.width = window.innerHeight / 25;
 
   newSprite.FRAMES = FRAMES;
   return  newSprite;
